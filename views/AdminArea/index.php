01dbbbf3982f0d7e72c8b425847f594e30dceb28
digamber <digamberpradhan89@gmail.com>
2020-02-27 17:25:05 +0545

    Admin page

--- views/AdminArea/index.php
+++ views/AdminArea/index.php
@@ -11,14 +11,14 @@
     <h2 class="nav-tab-wrapper">
 		<?php do_action( 'add_settings_tab' ); ?>
         <a href="<?php echo add_query_arg( array(
-			'page' => $this->plugin_url,
+			'page' => $this->admin_page_url,
 			'tab'  => 'settings'
 		), admin_url( 'admin.php' ) ); ?>"
            class="nav-tab <?php if ( ! ! empty( $_GET['tab'] ) || 'settings' === $_GET['tab'] ): ?> nav-tab-active<?php endif; ?>">
 			<?php _e( 'Settings 1', 'plugin-text-domain' ); ?>
         </a>
         <a href="<?php echo add_query_arg( array(
-			'page' => $this->plugin_url,
+			'page' => $this->admin_page_url,
 			'tab'  => 'check-balance'
 		), admin_url( 'admin.php' ) ); ?>"
            class="nav-tab <?php if ( ! empty( $_GET['tab'] ) && 'check-balance' === $_GET['tab'] ): ?> nav-tab-active<?php endif; ?>">
