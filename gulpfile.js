046e006c68a68b647f4fe4c36c1b99858d01ed8a
digamber pradhan <digamberpradhan89@gmail.com>
2020-06-29 17:16:32 +0545

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,12 @@
 const {src, dest, watch, parallel, series} = require('gulp');
 const wpPot = require('gulp-wp-pot');
-
 function generate_pot_file() {
-    return src(['src/**/*.php', 'views/**/*.php'])
+    return src(['**/*.php','!vendor/**/*.php'])
         .pipe(wpPot({
             domain: 'plugin-text-domain',
             package: 'Digthis Package'
         }))
-        .pipe(dest('languages/plugin-base.pot'));
+        .pipe(dest('languages/plugin-text-domain.pot'));
 }
 
-exports.generate_pot = generate_pot_file;
+exports.generate_pot = generate_pot_file
