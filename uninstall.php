04fd388405fdab67fb9ddfa643e771cb4ce9f744
digamber <digamberpradhan89@gmail.com>
2020-12-30 17:42:22 +0545

    A bit of house keeping

--- uninstall.php
+++ uninstall.php
@@ -3,5 +3,10 @@
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	die;
 }
+$option_keys = [
+	'digthis_plugin_activate'
+];
 
-delete_option('digthis_plugin_activate');
+foreach ( $option_keys as $option_key ) {
+	delete_option( 'digthis_plugin_activate' );
+}
