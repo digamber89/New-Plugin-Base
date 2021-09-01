45d9e725b4868bfb7f5b8f895d14974f6582dfff
digamber <digamberpradhan89@gmail.com>
2021-09-01 13:01:23 +0545

    Update to require PHP 5.6

--- plugin-base.php
+++ plugin-base.php
@@ -1,13 +1,15 @@
 <?php
 /*
-Plugin Name: Plugin Base Digamber
-Description: My Plugin Description Goes Here
-Plugin URI: URI goes here
-Author: Digamber Pradhan
-Author URI: https://www.digamberpradhan.com/
-Version: 1.0.0
-License: http://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html
-Text Domain: plugin-base
+ * Plugin Name: Plugin Base Digamber
+ * Description: My Plugin Description Goes Here
+ * Plugin URI: URI goes here
+ * Author: Digamber Pradhan
+ * Author URI: https://www.digamberpradhan.com/
+ * Version: 1.0.0
+ * Requires at least: 5.6
+ * Requires PHP:      7.2
+ * License: http://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html
+ * Text Domain: plugin-base
 */
 
 defined( 'ABSPATH' ) or die( 'Script Kiddies Go Away' );
